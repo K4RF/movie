b3257622b53de33eae935bda157f9e9edcb2dc59
K4RF <rpdirwk1025@naver.com>
2022-11-19 05:55:16 +0900

    Update MemberService.java

--- src/main/java/db/project/movie/service/MemberService.java
+++ src/main/java/db/project/movie/service/MemberService.java
@@ -3,13 +3,8 @@ package db.project.movie.service;
 import db.project.movie.constant.Role;
 import db.project.movie.dto.MemberDto;
 import db.project.movie.entity.MemberEntity;
-import db.project.movie.entity.MovieEntity;
-import db.project.movie.entity.ReviewEntity;
 import db.project.movie.repository.MemberRepository;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.Member;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -26,10 +21,12 @@ public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
-    public MemberEntity create(MemberDto memberDto){
+    public MemberEntity create(MemberDto memberDto, PasswordEncoder passwordEncoder){
         MemberEntity memberEntity = memberDto.toEntity();
         validateDuplicateMember(memberEntity);
+        String password = passwordEncoder.encode(memberEntity.getPassword());
         memberEntity.setRole(Role.ADMIN);
+        memberEntity.setPassword(password);
         return memberRepository.save(memberEntity);
     }
 
